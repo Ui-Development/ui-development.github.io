ffb1051fde084b499a46bfeb036601d4958df4bf
吉田保貴 <87916032+yoshidayasutaka@users.noreply.github.com>
2022-07-12 12:01:54 +0900

    Add files via upload

---
+++ test20220712/share/js/analytics.js
@@ -0,0 +1,6 @@
+document.write('<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-XJS1RR1JC3"></script>');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XJS1RR1JC3');
